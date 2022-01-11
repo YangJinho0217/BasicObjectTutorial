ff517d5ad3bd24a5c3d15ddb42088110592a2c24
YagSill <73631818+YangJinho0217@users.noreply.github.com>
2022-01-12 08:53:24 +0900

    Initial Commit

---
+++ BasicObjectTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicObjectTutorial
+//
+//  Created by 양진호 on 2022/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
