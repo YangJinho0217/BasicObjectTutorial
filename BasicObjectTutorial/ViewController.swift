0625f237e541b422632a586786592f6d9b412f99
YagSill <73631818+YangJinho0217@users.noreply.github.com>
2022-01-12 09:07:19 +0900

    commit

--- BasicObjectTutorial/ViewController.swift
+++ BasicObjectTutorial/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Text: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Text.text = "텍스트 변경후"
     }
 
 
